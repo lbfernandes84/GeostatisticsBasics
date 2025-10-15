ca74d69fc3b67884e7f31460b63c75dd2bacabb4
Lucas Fernandes <lucas.fernandes@dataminesoftware.com>
2025-10-15 12:09:19 -0300

    Project organization before move to python part

---
+++ DllWrapper/include/EntitiesWrapper.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "DllWrapper_API.h"
+#include "Entities/Point.h"
+#include "Entities/SampleSet.h"
+#include "Entities/SampleValues.h"
+
+extern "C"
+{
+	//Point
+	DLLWRAPPER_API Point* EntitiesPoint_Create(double x, double y, double z);
+
+	//SampleSet
+	DLLWRAPPER_API SampleSet* EntitiesSampleSet_Create();
+	DLLWRAPPER_API void EntitiesSampleSet_AddSample(SampleSet* pSampleSet, Point* pPoint);
+
+	//SampleValues
+	DLLWRAPPER_API SampleValues* EntitiesSampleValues_Create(const int& size);
+	DLLWRAPPER_API void EntitiesSampleValues_Setvalue(SampleValues* pSampleValues, const int& index, double value);
+	
+}
---
+++ DllWrapper/src/EntitiesWrapper.cpp
@@ -0,0 +1,36 @@
+#include "../include/EntitiesWrapper.h"
+
+Point* EntitiesPoint_Create(double x, double y, double z)
+{
+	auto* p = new Point();
+	p->x = x;
+	p->y = y;
+	p->z = z;
+	return p;
+}
+
+SampleSet* EntitiesSampleSet_Create()
+{
+	return new SampleSet();
+}
+
+void EntitiesSampleSet_AddSample(SampleSet* pSampleSet, Point* pPoint)
+{
+	if (pSampleSet && pPoint)
+	{
+		pSampleSet->AddSample(*pPoint);
+	}
+}
+
+SampleValues* EntitiesSampleValues_Create(const int& size)
+{
+	return new SampleValues(size);
+}
+
+void EntitiesSampleValues_Setvalue(SampleValues* pSampleValues, const int& index, double value)
+{
+	if (pSampleValues)
+	{
+		pSampleValues->SetValue(index, value);
+	}
+}
